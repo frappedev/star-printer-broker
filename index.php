92940bc5e0e83e66593f349cd854fee9812f739d
Harpreet Singh <thesinghharpreet@gmail.com>
2020-09-23 17:36:37 +0530

    Set diaply errors

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once 'vendor/autoload.php';
+ini_set('display_errors', 1);
+
+require_once __DIR__.'/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
